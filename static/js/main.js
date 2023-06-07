8ca7bff8b8ef53fd75035eb167dc184fa9bf4290
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-06-07 16:55:11 +0900

    은희님 폴더구조 수정 및 지영 작업 반영

--- static/js/main.js
+++ static/js/main.js
@@ -9,7 +9,7 @@ const fetchMovieData = async () => {
                 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMzFhOWNiZmI5ZmQyYjM0Zjc5NDJhMDE3ZDQzNzk1NCIsInN1YiI6IjY0NzA4OTQ1MTNhMzIwMDEzMzg2MDdhZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IRZX6ubYrGSryWyuwy-pz7rwGMOmnbvU9PitigtZTcM',
         },
     };
-
+    
     const response = await fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options);
 
     const data = await response.json();
